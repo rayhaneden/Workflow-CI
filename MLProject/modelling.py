4362b12c0fb91d88c501fe37d9c39871f5bab8b5
rayhaneden <104288091+rayhaneden@users.noreply.github.com>
2025-06-20 15:35:00 +0700

    Update modelling.py
    mengubah agar run MLflow tidak terjadi konflik

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -22,25 +22,25 @@ X = data.drop(columns=["first_winner"])
 y = data["first_winner"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-mlflow.set_experiment("Pokemon_Battle_Prediction_CI")
 
-with mlflow.start_run():
-    mlflow.sklearn.autolog()
-    
-    model = RandomForestClassifier(
-        n_estimators=args.n_estimators,
-        max_depth=max_depth,
-        min_samples_leaf=args.min_samples_leaf,
-        min_samples_split=args.min_samples_split,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
-    
-    y_pred = model.predict(X_test)
-    
-    mlflow.log_metric("test_accuracy", accuracy_score(y_test, y_pred))
-    mlflow.log_metric("test_f1_score", f1_score(y_test, y_pred))
-    mlflow.log_metric("test_precision", precision_score(y_test, y_pred))
-    mlflow.log_metric("test_recall", recall_score(y_test, y_pred))
+# Aktifkan autolog (MLflow Project sudah mengatur run-nya)
+mlflow.sklearn.autolog()
 
-    print("Model training selesai dengan parameter optimal hasil tuning!")
+model = RandomForestClassifier(
+    n_estimators=args.n_estimators,
+    max_depth=max_depth,
+    min_samples_leaf=args.min_samples_leaf,
+    min_samples_split=args.min_samples_split,
+    random_state=42
+)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# Log metrik test set secara manual
+mlflow.log_metric("test_accuracy", accuracy_score(y_test, y_pred))
+mlflow.log_metric("test_f1_score", f1_score(y_test, y_pred))
+mlflow.log_metric("test_precision", precision_score(y_test, y_pred))
+mlflow.log_metric("test_recall", recall_score(y_test, y_pred))
+
+print("Model training selesai dengan parameter optimal hasil tuning!")
